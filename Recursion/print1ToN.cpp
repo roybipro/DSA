ef0227f60578361f07f1340c487d1a9c92999c20
Bipro Roy <roybipro896@gmail.com>
2025-07-21 21:36:43 +0600

    add backtrack code

---
+++ Recursion/print1ToN.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+  void f(int i,int n){
+    if (i>n){
+        return;
+    }
+    cout<<i<<endl;
+    f(i+1,n);
+  }
+//Revarse order
+  void fRevarse(int i,int n){
+    if (i<1){
+        return;
+    }
+    cout<<i<<endl;
+    fRevarse(i-1,n);
+  }
+  //Backtracking 
+  void fBacktrack(int i,int n){
+    if (i<1){
+        return;
+    }
+    fBacktrack(i-1,n);
+    cout<<i<<endl; 
+  }
+
+  int main() {
+   int n;
+   cin>>n;
+   fBacktrack(n,n); 
+    return 0;
+}
