69d0b989dccdbb1167de6381a66e56ba760fc43b
Roy Bipro <roybipro896@gmail.com>
2025-10-19 05:44:12 +0600

    hh

--- array/cahr.cpp
+++ array/cahr.cpp
@@ -4,7 +4,10 @@ using namespace std;
 int main()
 {
     char arr[4]={'a','b','c'};
-    for(int i=0;i<=sizeof(arr)/sizeof(arr[0]);i++){
-    cout<<arr[i];}
+    for(int i=0;i<sizeof(arr)/sizeof(arr[0]);i++){
+        cout<<arr[i];
+        
+    }
+
     return 0;
 }
