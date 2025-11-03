1042acdf1bec3a7cd8aab3354995dbba8d87d03f
Roy Bipro <roybipro896@gmail.com>
2025-11-03 10:39:02 +0600

    .

--- sorting/Selection Sort/ex1.cpp
+++ sorting/Selection Sort/ex1.cpp
@@ -2,62 +2,32 @@
 #include <iostream>
 using namespace std;
 // Sorting Function
-void selectionSort(int arr[], int n)
-{
-    int i, j, min_index;
-    // one by one move boundry of
-    // unsorted array
-    /*/ for (i = 0; i < n - 1; i++)
-     {
-         // Find the minimum element in unsorted array
-         min_index=i;
-         for(j=i+1;j<n;j++){
-             if(arr[j]<arr[min_index])
-             min_index=j;
-
-           }
-           // Swap the found minimum element
-         // with the first element
-     if(min_index!=i)
-     swap(arr[min_index],arr[i]);
-     }
-     */
-    for (i = 0; i < n - 1; i++)
-    {
-        // Find the minimum element in unsorted array
-        for (j = i + 1; j < n; j++)
-        {
-            if (arr[j] < arr[i])
-            // Swap the found minimum element
-            // with the first element
-            {
-                int temp = arr[j];
-                arr[j] = arr[i];
-                arr[i] = temp;
+void selection_sort(int arr[], int n)
+{  
+    for(int i=0;i<=n-2;i++){
+        int mini =i; 
+        for(int j=i;j<=n-1;j++){
+            if(arr[j]<arr[mini]){
+                mini = j;
             }
         }
-    }
-}
-
-void printArray(int arr[], int size)
-{
-    int i;
-    for (i = 0; i < size; i++)
-    {
-        cout << arr[i] << " ";
-        cout << endl;
+        int temp =arr[mini];
+        arr[mini]=arr[i];
+        arr[i]=temp;
     }
 }
 
 // Driver program
 int main()
 {
-    int arr[] = {64, 25, 12, 22, 11};
-    int n = sizeof(arr) / sizeof(arr[0]);
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++) cin>>arr[i];
+    selection_sort(arr,n);
+    for (int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
 
-    // Function Call
-    selectionSort(arr, n);
-    cout << "Sorted array: \n";
-    printArray(arr, n);
     return 0;
 }
