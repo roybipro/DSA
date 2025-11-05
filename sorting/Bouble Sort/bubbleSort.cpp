42f802481a1050442d6cf496d8fca3a3f2124219
Roy Bipro <roybipro896@gmail.com>
2025-11-05 23:05:06 +0600

    optimize the bubble sort

--- sorting/Bouble Sort/bubbleSort.cpp
+++ sorting/Bouble Sort/bubbleSort.cpp
@@ -25,13 +25,16 @@ void printArray(int arr[], int size) {
 
 void bubbleSort(int arr[], int n) {
     for (int i = n-1; i>=0; i--) {
+        int didSwap=0;
         for (int j = 0; j <i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
                 int temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
+                didSwap=1;
             }
         }
+        if (didSwap==0) break;
     }
 }
 int main() {
