1ff8ffa15e419e6891d0b3ffd2615c284cb80ed2
Roy Bipro <roybipro896@gmail.com>
2025-11-16 12:37:46 +0600

    doing hash array tuf problem

--- Hash Tables/HashingTUF.cpp
+++ Hash Tables/HashingTUF.cpp
@@ -2,24 +2,29 @@
 using namespace std;
 //hashing means prestoring and fetching
 int main() {
-    // Your code goes here
     int n;
-    cin>>n;
+    cout << "Size of array :";
+    cin >> n;
     int arr[n];
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
+    cout<<"Enter the eliments :";
+    for(int i =0;i<n;i++){
+        cin>> arr[i];
     }
+
     //precompute
-    int hash[13]={0};
+    int hash[13] ={0};
     for(int i=0;i<n;i++){
-        hash[arr[i]]+=1;
+       hash[arr[i]]+= 1; 
     }
+
     int q;
-    cin>>q;
+    cout<<"Enter what you want to search :";
     while(q--){
         int number;
         cin>>number;
+        //fetch
         cout<<hash[number]<<endl;
+
     }
     return 0;
 }
