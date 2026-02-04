0d23343a9b83c1da1011bb0826f75813f49f56e0
Roy Bipro <roybipro896@gmail.com>
2026-02-04 21:38:37 +0600

    learn array pointer

---
+++ pointers/arrayPointer.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // arr is a constant pointer
+    int arr[]={1,2,3,4,5};
+
+    cout<<arr<<endl; //arr (name of the array) point to the index 0 memory adress
+    cout<<*arr<<endl;
+    return 0;
+}
