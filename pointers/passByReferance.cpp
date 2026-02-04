61191a28bf92f4c4363c4393b6f7cbdad13fe353
Roy Bipro <roybipro896@gmail.com>
2026-02-04 21:25:30 +0600

    make pass by referance

--- pointers/passByReferance.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    // Your code goes here
-    
-    return 0;
-}
---
+++ pointers/passByReference.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+void changeA(int* ptr){ //pass by reference 
+    *ptr=20;
+}
+int main() {
+    
+    int a =10;
+
+    changeA(&a);
+
+    cout<<"Inside main fnx :"<<a<<endl;//20
+    return 0;
+}
