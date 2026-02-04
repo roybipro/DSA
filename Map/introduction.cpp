a6336b467f907d1a2195ea75c17d2d9b0e0914fc
Roy Bipro <roybipro896@gmail.com>
2026-02-04 21:05:28 +0600

    learn vectors

--- Map/introduction.cpp
+++ Map/introduction.cpp
@@ -2,7 +2,7 @@
 #include<unordered_map>
 #include<list>
 using namespace std;
-
+template <typename T>
 class graph{
     public:
         unordered_map<int, list<int> > adj;
@@ -12,12 +12,42 @@ class graph{
             //direction = 1 -> directed
 
             //create an edge from u to v
-
+            adj[u].push_back(v);
+            if(direction==0)
+            {
+              adj [v].push_back(u);  
+            }
         }
+
+        void printAdjList(){
+    for(auto &i : adj){
+        cout << i.first << " -> ";
+        for(auto &j : i.second){
+            cout << j << ", ";
+        }
+        cout << endl;
+    }
 }
+};
 
 int main() {
     // Your code goes here
+    int n;
+    cout <<"Enter the number of nodes"<<endl;
+    cin>>n;
     
+    int m;
+    cout<<"Enter the number of edges "<<endl;
+    cin>>m;
+
+    graph<int> g;
+    for(int i=0;i<m;i++){
+        int u,v;
+        cin>>u>>v;
+        //creating an undurected graph
+        g.addEdge(u,v,0);
+    }
+    //print graph
+    g.printAdjList();
     return 0;
 }
